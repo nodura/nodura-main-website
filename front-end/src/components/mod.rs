392ea81be5f1d0db80aca2b5bab6a1c6cd763334
SanderLoman <sanderfeitsma13@gmail.com>
2024-09-01 01:10:30 +0200

    chore: got tailwind working

--- front-end/src/components/mod.rs
+++ front-end/src/components/mod.rs
@@ -1,11 +1,1 @@
 use yew::prelude::*;
-
-#[function_component(Hello)]
-pub fn hello() -> Html {
-    html! {
-        <div>
-            <h1>{"Hello from a separate file!"}</h1>
-            <p>{"This is a Yew component in a separate file."}</p>
-        </div>
-    }
-}
