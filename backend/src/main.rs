5a860f80a74b6d7d5a525079eb5bf3ba5df7626d
SanderLoman <sanderfeitsma13@gmail.com>
2024-09-15 23:17:00 +0200

    chore: minor changes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,18 @@
-use actix_web::{App, HttpServer};
-use tracing::{info, Level};
+use actix_web::{get, web, App, HttpServer, Responder};
+use tracing::Level;
 use tracing_subscriber;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    web::Json("Hello from Actix-web!")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
 
-    // !!!
-    // Comment out the following lines because we dont have services yet
-    //
-    // HttpServer::new(|| {
-    //     App::new()
-    //         .service(/* your services here */)
-    // })
-    // .bind("127.0.0.1:8080")?
-    // .run()
-    // .await
-    Ok(())
+    HttpServer::new(|| App::new().service(hello))
+        .bind("127.0.0.1:8081")?
+        .run()
+        .await
 }
